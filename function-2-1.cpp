8032e1d048386d31d086660edf5e97ff9ffd46b0
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-07-25 18:28:25 +0930

    test

---
+++ function-2-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int min_element(int array[], int n) {
+
+    if (n <1) {
+        return 0;
+    }
+
+    int min = 0;
+
+    for (int i=0; i<n; i++) {
+
+        if (min > array[i]) {
+        min = array[i];
+        }
+    }
+
+    return min;
+
+}
