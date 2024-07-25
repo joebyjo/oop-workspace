058b052dbc02a20cb01c8493e9ddd561bc715046
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-07-25 18:00:40 +0930

    testing main

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-using namespace std;
 
-int main()
-{
+extern int array_sum(int array[], int n);
+
+int main() {
 
     int number[] = {1,2,3,4,5};
+    
 
     
     return 0;
