25b92caa6367046f8f69327de42a24243ea0b188
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-07-26 16:13:40 +0930

    Medium qs

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -8,7 +8,7 @@ void two_five_nine(int array[], int n) {
     int two =0, five=0, nine=0;
 
 
-    for (int i=1; i<n; i++){
+    for (int i=0; i<n; i++){
         switch (array[i]) {
             case 2:
                 two++;
