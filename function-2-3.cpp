af7d4d023f7088dd6af00ba386f8ac094397de76
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-07-26 17:41:07 +0930

    Tricky qs

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -5,17 +5,19 @@ using namespace std;
 
 void two_five_nine(int array[], int n) {
 
-    int two =0, five=0, nine=0;
-
+    int two=0, five=0, nine=0;
 
     for (int i=0; i<n; i++){
         switch (array[i]) {
             case 2:
                 two++;
+                break;
             case 5:
                 five++;
+                break;
             case 9:
                 nine++;
+                break;
         };
     }
 
