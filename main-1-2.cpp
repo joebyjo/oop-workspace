19951d270cf05d064ff6ad55f408f49c52e89709
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-08-01 18:49:00 +0930

    easy qs

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 
-extern int sum_diagonal(int array[4][4]);
-
-using namespace std;
-
-
-int main(void) {
-
-    int array[4][4] = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-
-    int sum = sum_diagonal(array);
-
-    cout << sum << endl;
-
-
+int main() {
+    return 0;
 }
