fd4a2a3d4e374f9dcbc5e68b0b0b4d4992d5d241
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-08-07 17:27:46 +0930

    All answers

--- main.cpp
+++ main.cpp
@@ -9,23 +9,29 @@ using namespace std;
 int main() {
 
 
-    double x = 1.23;
-    char y = 'a';
+    // double x = 1.23;
+    // char y = 'a';
 
-    double *x_pointer = &x;
-    char *y_pointer = &y;
+    // double *x_pointer = &x;
+    // char *y_pointer = &y;
 
-    cout << *x_pointer << endl << *y_pointer << endl;
+    // cout << *x_pointer << endl << *y_pointer << endl;
 
-    changeValue(x_pointer);
+    // changeValue(x_pointer);
 
-    cout << *x_pointer << endl;
+    // cout << *x_pointer << endl;
 
 
-    double array[] = {1,2,3};
+    // double array[] = {1,2,3};
+
+
+    double* array = dynamicArray(3,4);
 
     printArray(array, 3);
 
+    cout << "Max is: " << arrayMax(array,3) << endl;
+
+    delete[] array;
 
 
 
--- workshop.h
+++ workshop.h
@@ -15,10 +15,27 @@ void printArray(double* array, int n) {
 
 }
 
+double arrayMax(double* array, int n) {
+    double max = array[0];
+    for (int i=1; i<n; i++) {
+        if (array[i]> max) {
+            max = array[i];
+        }
+    }
 
-double* dynamicArray(int n) {
+    return max;
+}
+
+
+
+
+double* dynamicArray(int n, double M) {
 
     double* array = new double[n]; 
+
+    for (int i=0;i<n; i++) {
+        array[i] = M;
+    }
 
     return array;
 }
