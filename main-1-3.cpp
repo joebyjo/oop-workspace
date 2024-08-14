afc8bcf0672fafe05e526b4d69c9ee6023ec6fa3
joebyjoedu <a1912873@student.adelaide.edu.au>
2024-08-14 13:27:10 +0930

    checking q 3

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -4,16 +4,27 @@
 using namespace std;
 
 extern PersonList createPersonList(int n);
+extern PersonList deepCopyPersonList(PersonList pl);
 
 int main() {
 
-    int n = 2;
+    // int n = 2;
 
-    PersonList array= createPersonList(n);
+    // PersonList array1;
 
-    for (int i=0; i<array.numPeople; i++) {
-        cout << array.people[i].name << "   " << array.people[i].age << endl;
-    }
+    // Person x;
+    // x.name = "Joe";
+    // x.age = 19;
+
+    // array1.numPeople =1;
+    // array1.people[0] = x;
+
+    // PersonList array2= deepCopyPersonList(array1);
+    
+
+    // for (int i=0; i<array2.numPeople; i++) {
+    //     cout << array2.people[i].name << "   " << array2.people[i].age << endl;
+    // }
 
     return 0;
 }
