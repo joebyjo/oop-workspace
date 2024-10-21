88b43f31b8bd3d70ae5e885a4b0a41785a7ff2fa
joebyjo <joebyjo@gmail.com>
2024-10-21 20:59:47 +1030

    q2

--- Ship.h
+++ Ship.h
@@ -15,8 +15,8 @@ Ship:: Ship(int x, int y): GameEntity(x,y,'S') {
 
 
 void Ship::move(int dx, int dy) {
-    get<0>(position) = dx;
-    get<1>(position) = dy;
+    get<0>(position) += dx;
+    get<1>(position) += dy;
 };
 
 
