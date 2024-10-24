4a15f20e3da04dffa56879b1896a14c2dbd59c57
joebyjo <joebyjo@gmail.com>
2024-10-24 17:37:06 +1030

    q2

--- Robot.hpp
+++ Robot.hpp
@@ -8,15 +8,40 @@ class Robot: public GridItem {
     private:
         int health;
 
+
     public:
         Robot(int gridWidth, int gridHeight);
-        int getHealth() // gets the current health of the robot
-        void takeHit() // decrements the health of the robot. The robot cannot have negative health
-        bool move(int xOffset, int yOffset)
+        int getHealth(); // gets the current health of the robot
+        void takeHit(); // decrements the health of the robot. The robot cannot have negative health
+        bool move(int xOffset, int yOffset);
 
 };
 
+Robot:: Robot(int gridWidth, int gridHeight): GridItem(0,0,gridWidth,gridHeight) {
+    health = 3;
+}
+
+int Robot:: getHealth() {
+    return this->health;
+}
+void Robot:: takeHit() {
+    this->health--;
+    if (health < 0) {
+        this->health = 0;
+    }
+}
 
 
+bool Robot:: move(int xOffset, int yOffset) {
+    if (xOffset == 0 && (coords.first + xOffset <= width && coords.second + yOffset <= height)) {
+        coords.second += yOffset;
+        return true;
+    } else if (yOffset == 0 && (coords.first + xOffset <= width && coords.second + yOffset <= height)) {
+        coords.first += xOffset;
+        return true;
+    } 
+
+    return false;
+}
 
 #endif
